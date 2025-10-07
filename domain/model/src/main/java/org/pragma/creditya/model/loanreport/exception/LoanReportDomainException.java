1acf024bf28c561ebf8b1e0b43e1a2bc99688502
hans.arias <hansarias74@gmail.com>
2025-10-07 15:47:32 -0500

    first commit

---
+++ domain/model/src/main/java/org/pragma/creditya/model/loanreport/exception/LoanReportDomainException.java
@@ -0,0 +1,10 @@
+package org.pragma.creditya.model.loanreport.exception;
+
+import org.pragma.creditya.model.shared.domain.exception.DomainException;
+
+public class LoanReportDomainException extends DomainException {
+
+    public LoanReportDomainException(String message) {
+        super(message);
+    }
+}
--- domain/model/src/main/java/org/pragma/creditya/model/loanreport/valueobject/Count.java
+++ domain/model/src/main/java/org/pragma/creditya/model/loanreport/valueobject/Count.java
@@ -1,7 +1,17 @@
 package org.pragma.creditya.model.loanreport.valueobject;
 
-public record Count (int value) {
+import org.pragma.creditya.model.loanreport.exception.LoanReportDomainException;
+
+public record Count (Integer value) {
+
+    public Count {
+        if (value == null)
+            throw new LoanReportDomainException("Count must be mandatory");
+
+        if (value < 0)
+            throw new LoanReportDomainException("Count must be positive");
 
 
+    }
 
 }
